6485f5c633f98b0a8a23d8d49fadcd10ffc705a1
Daniel Corson <danl@alum.mit.edu>
2009-05-11 10:37:41 -0700

    pgn parsing working on 30k-line game collection

---
+++ src/Data/PGN.hs
@@ -0,0 +1,102 @@
+module Data.PGN (
+  PGN(..),
+  pgnParser
+) where
+
+import Control.Applicative hiding (many, optional, (<|>))
+import Control.Arrow
+import Control.Monad
+import Data.Char
+import Data.Maybe
+import Data.SAN
+import Text.Parsec
+import Text.Parsec.Language (emptyDef)
+import qualified Text.Parsec.Token as P
+
+data PGN = PGN {
+  pgnTags :: [Tag],
+  pgnMoves :: [(Move, Annotation)],
+  pgnResult :: Result
+  }
+  deriving (Eq, Show)
+
+type Tag = (String, String)
+
+data Result = WhiteWin | BlackWin | DrawnGame | OtherResult
+  deriving (Eq, Show)
+
+lexer :: P.TokenParser ()
+lexer = P.makeTokenParser emptyDef {
+  P.commentStart = "{",
+  P.commentEnd   = "}",
+  P.commentLine  = ";",
+  P.nestedComments = False
+  }
+
+-- Bind the lexical parsers at top-level.
+whiteSpace :: Parsec String () ()
+whiteSpace = P.whiteSpace lexer
+
+decimal :: Parsec String () Integer
+decimal = P.decimal lexer
+
+lexeme :: Parsec String () a -> Parsec String () a
+lexeme = P.lexeme lexer
+
+-- Parsec String () for a PGN symbol token.
+pgnSymbol :: Parsec String () String
+pgnSymbol = lexeme $ do
+  first <- alphaNum
+  rest <- many (alphaNum <|> oneOf "_+#=:-")
+  return (first:rest)
+
+-- Parsec String () for a PGN string token.
+pgnString :: Parsec String () String
+pgnString = lexeme $ between (char '"') (char '"') (many pgnStringChar)
+
+-- Parsec String () for characters inside a string token.
+pgnStringChar :: Parsec String () Char
+pgnStringChar = do
+  c <- satisfy (\c -> isPrint c && c /= '"')
+  if c == '\\'
+    then oneOf "\\\""
+    else return c
+
+pgnTagName :: Parsec String () String
+pgnTagName = lexeme . liftM2 (:) alphaNum . many $ alphaNum <|> char '_'
+
+pgnInteger :: Parsec String () Integer
+pgnInteger = lexeme decimal
+
+tagParser :: Parsec String () Tag
+tagParser = liftM2 (,)
+  (lexeme (char '[') >> pgnTagName)
+  (pgnString <* lexeme (char ']'))
+
+-- We should check the move numbers?  But what is best way to deal with
+-- misnumbered input anyway?
+resultOrMoveParser :: Parsec String () (Either Result (Move, Annotation))
+resultOrMoveParser = (string "*" >> return (Left OtherResult))
+  <|> do
+    num <- pgnInteger
+    case num of
+      0 -> (string "-1" >> return (Left BlackWin)) <|> moveAfterNum (Just num)
+      1 -> (string "-0" >> return (Left WhiteWin)) <|>
+        (string "/2-1/2" >> return (Left DrawnGame)) <|>
+        moveAfterNum (Just num)
+      _ -> moveAfterNum $ Just num
+  <|> (moveAfterNum =<< optionMaybe pgnInteger)
+  where
+  moveAfterNum (Just n) = lexeme (skipMany (char '.')) >> moveAfterNum Nothing
+  moveAfterNum Nothing  = Right <$> lexeme moveAnnParser
+
+stopOnLeft :: (Monad m) => m (Either d a) -> m (d, [a])
+stopOnLeft p = p >>= \ res -> case res of
+  Left stopRes -> return (stopRes, [])
+  Right rRes -> second (rRes:) `liftM` stopOnLeft p
+
+pgnParser :: Parsec String () PGN
+pgnParser = whiteSpace >> do
+  tags <- many tagParser
+  (result, moves) <- stopOnLeft resultOrMoveParser <* whiteSpace
+  return $ PGN tags moves result
---
+++ src/Data/SAN.hs
@@ -0,0 +1,69 @@
+module Data.SAN (
+  Move(..),
+  Annotation,
+  Piece,
+  moveAnnParser
+) where
+
+import Control.Applicative hiding (many, (<|>))
+import Control.Monad
+import Data.Char
+import Text.Parsec
+
+data Move = Move {
+  mvPiece :: Maybe Piece,
+  mvFromX :: Maybe Int,
+  mvFromY :: Maybe Int,
+  mvIsACapture :: Bool,
+  mvToX :: Int,
+  mvToY :: Int,
+  mvPromote :: Maybe Piece
+  } |
+  Castle Piece
+  deriving (Show, Ord, Eq)
+
+type Annotation = String
+
+type Piece = Char
+
+posIntParser :: Parsec String () Int
+posIntParser = read <$> many1 digit
+
+castlingParser :: Parsec String () Move
+castlingParser = string "O-O" >>
+  option (Castle 'K') (string "-O" >> return (Castle 'Q'))
+
+annotationParser :: Parsec String () Annotation
+annotationParser = many $ oneOf "!?+#"
+
+colParser :: Parsec String () Int
+colParser = (\ x -> ord x - ord 'a' + 1) <$> lower
+
+moveParser :: Parsec String () Move
+moveParser = castlingParser <|> do
+  pieceMb <- optionMaybe upper
+  col1Mb <- optionMaybe colParser
+  row1Mb <- optionMaybe posIntParser
+  case (pieceMb, col1Mb) of
+    (Nothing, Nothing) -> fail "missing starting piece or column"
+    _ -> return ()
+  isCap <- option False $ char 'x' >> return True
+  col2Mb <- optionMaybe colParser
+  row2Mb <- optionMaybe posIntParser
+  (col1Mb', row1Mb', col2, row2) <-
+    case (isCap, col1Mb, row1Mb, col2Mb, row2Mb) of
+      (True, _, _, Nothing, _) -> fail "missing ending column for capture"
+      (True, _, _, _, Nothing) -> fail "missing ending row for capture"
+      (False, Nothing, Nothing, Nothing, _) -> fail "missing starting column"
+      (False, Nothing, Nothing, _, Nothing) -> fail "missing starting row"
+      (False, Just col1, Just row1, Nothing, Nothing) ->
+        return (Nothing, Nothing, col1, row1)
+      (_, col1Mb, row1Mb, Just col2, Just row2) ->
+        return (col1Mb, row1Mb, col2, row2)
+      _ -> fail "impossible"
+  promote <- optionMaybe $ char '=' >> upper
+  ann <- optionMaybe annotationParser
+  return $ Move pieceMb col1Mb' row1Mb' isCap col2 row2 promote
+
+moveAnnParser :: Parsec String () (Move, Annotation)
+moveAnnParser = liftM2 (,) moveParser annotationParser
